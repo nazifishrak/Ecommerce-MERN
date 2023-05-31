21e69cfad8966534a374cea16d953fdf10cfea79
nazifishrak <nzfishrak60@gmail.com>
2023-05-31 20:10:04 +0600

    added basic test routes

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,25 @@
 //Creating express app
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const PORT = 3000;
 
+app.use(morgan("dev"));
+app.get("/test", (req, res) => {
+  res.status(200).send({message: "GET: api working properly"});
+});
 
-app.get("/products", (req, res)=>{
-    res.send("products are returned")
-})
-app.listen(PORT, ()=>{
-    console.log(`server listening at port ${PORT}`);
+app.post("/test", (req, res) => {
+    res.status(200).send({message: "POST: api working properly"});
+  });
+
+app.put("/test", (req, res) => {
+  res.status(200).send({message: "PUT: api working properly"});
+});
+
+app.delete("/test", (req, res) => {
+    res.status(200).send({message: "DEL: api working properly"});
+  });
+app.listen(PORT, () => {
+  console.log(`server listening at port ${PORT}`);
 });
